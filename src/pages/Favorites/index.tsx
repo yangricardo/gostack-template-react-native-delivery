a9d95e7469c362dcafea6909ba9ec58cb85948aa
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2020-12-21 22:00:09 -0300

    load orders

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,6 +32,11 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
+      const response = await api.get('/favorites');
+      setFavorites(response.data.map((favorite: Food)=>({
+        ...favorite,
+        formattedPrice: formatValue(favorite.price)
+      })))
       // Load favorite foods from api
     }
 
